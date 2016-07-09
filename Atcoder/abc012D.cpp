48a247e494e26e936c3969601f5e1f2a7c378ec5
unknown <>
2016-07-09 17:31:25 +0900

    atcoder

--- Atcoder/abc012D.cpp
+++ Atcoder/abc012D.cpp
@@ -12,7 +12,10 @@ int Basute[N + 1][N + 1];    //それぞれの点の最大値を格納する
 int Basute2[N + 1];          //バス停のその時点での値を格納する
 bool Visit[N + 1][N + 1];    //訪れたかどうかのフラグ
 
-void func(int n, int x, int count, int eki);
+//待ち行列
+int Q[N + 1];
+int sp, ep;
+
 int main()
 {
 	int n, m;
@@ -33,61 +36,19 @@ int main()
 		}
 	}
 
-
-	for (int z = 1; z <= n; z++) {
-		Basute[z][z] = 0;
-		for (int a = z; a <= n; a++) {
-			for (int b = 1; b <= n; b++) {
-				if (Basu[a][b] && !Visit[a][b]) {
-					if (Basute[z][b] > Time[a][b] + Basute2[a]) {
-						Basute[z][b] = Time[a][b] + Basute2[a];
-						Basute2[b] = Basute[z][b];
+	for (int a = 1; a <= n; a++) {
+		for (int b = 1; b <= n; b++) {
+			for (int c = 1; c <= n; c++) {
+				if (Basu[b][c] && !Visit[b][c]) {
+					if (Basute[a][c] > Time[b][c] + Basute2[b]) {
+						Basute[a][c] = Time[b][c] + Basute2[b];
 					}
+					Q[ep++] = c;
 				}
 			}
 		}
-		/*
-		for (int c = 1; c < z; c++) {
-			for (int d = 1; d <= n; d++) {
-				if (Basu[c][d] && !Visit[c][d]) {
-					if (Basute[c][d] && !Visit[c][d]) {
-						if (Basute[z][d] > Time[c][d] + Basute2[c]) {
-							Basute[z][d] = Time[c][d] + Basute2[c];
-							Basute2[d] = Basute[z][d];
-						}
-					}
-				}
-			}
-		}
-		*/
-		for (int f = z - 1; f >= 1; f--) {
-			for (int g = 1; g <= n; g++) {
-				if (Basu[f][g] && !Visit[f][g]) {
-					if (Basute[z][g] > Time[f][g] + Basute2[f]) {
-						Basute[z][g] = Time[f][g] + Basute2[f];
-						Basute2[g] = Basute[z][g];
-					}
-				}
-			}
-		}
-		/*
-		for (int h = n; h >= z; h--) {
-			for (int i = 1; i <= n; i++) {
-				if (Basu[h][i] && !Visit[h][i]) {
-					if (Basute[z][i] > Time[h][i] + Basute2[h]) {
-						Basute[z][i] = Time[h][i] + Basute2[h];
-						Basute2[i] = Basute[z][i];
-					}
-				}
-			}
-		}
-		*/
-		for (int i = 1; i <= n; i++) {
-			Basute2[i] = 0;
-		}
-
 	}
-
+	
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			cout << Basute[i][j] << " ";
@@ -167,8 +128,7 @@ int main()
 	return 0;
 }
 */
-/*
-#include <iostream>
+/*#include <iostream>
 #include <cmath>
 #include <algorithm>
 using namespace std;
@@ -185,70 +145,65 @@ bool Visit[N + 1][N + 1];    //訪れたかどうかのフラグ
 void func(int n, int x, int count, int eki);
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	//バス停a,bとaからbまでにかかる時間
-	for (int i = 0; i < m; i++) {
-		int a, b, time;
-		cin >> a >> b >> time;
-		Time[a][b] = time;
-		Time[b][a] = time;
-		Basu[a][b] = true;
-		Basu[b][a] = true;
-	}
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			Basute[i][j] = 1000000;
-		}
-	}
-
-	for (int a = 1; a <= n; a++) {
-		for (int b = a; b <= n; b++) {
-			for (int c = 1; c <= n; c++) {
-				if (Basu[b][c] && !Visit[b][c]) {
-					if (Basute[a][c] > Time[b][c] + Basute2[b]) {
-						Basute[a][c] = Time[b][c] + Basute2[b];
-						Basute2[c] = Basute[a][c];
-					}
-					Visit[b][c] = true;
-					Visit[c][b] = true;
-				}
-			}
-		}
-
-		for (int d = 1; d < a; d++) {
-			for (int e = 1; e < a; e++) {
-				if (Basu[d][e] && !Visit[d][e]) {
-					if (Basute[a][e] > Time[d][e] + Basute2[d]) {
-						Basute[a][e] = Time[d][e] + Basute2[d];
-						Basute2[d] = Basute[a][e];
-						Visit[d][e] = true;
-						Visit[e][d] = true;
-					}
-				}
-			}
-		}
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				Visit[i][j] = false;
-			}
-		}
-
-		for (int i = 1; i <= n; i++) {
-			Basute2[i] = 0;
-		}
-	}
-
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cout << Basute[i][j] << " ";
-		}
-		cout << endl;
-	}
-
-	return 0;
+int n, m;
+cin >> n >> m;
+//バス停a,bとaからbまでにかかる時間
+for (int i = 0; i < m; i++) {
+int a, b, time;
+cin >> a >> b >> time;
+Time[a][b] = time;
+Time[b][a] = time;
+Basu[a][b] = true;
+Basu[b][a] = true;
 }
-*/
+
+for (int i = 1; i <= n; i++) {
+for (int j = 1; j <= n; j++) {
+Basute[i][j] = 1000000;
+}
+}
+
+
+for (int z = 1; z <= n; z++) {
+Basute[z][z] = 0;
+
+for (int i = 1; i <= n; i++) {
+if (Basu[z][i] && !Visit[z][i]) {
+Basute[z][i] = Time[z][i];
+Basute2[i] = Time[z][i];
+Visit[z][i] = true;
+Visit[i][z] = true;
+}
+}
+for (int a = 1; a <= n; a++) {
+for (int b = 1; b <= n; b++) {
+if (Basu[a][b] && !Visit[a][b]) {
+if (Basute[z][b] > Time[a][b] + Basute2[a]) {
+Basute[z][b] = Time[a][b] + Basute2[a];
+Basute2[b] = Basute[z][b];
+Visit[a][b] = true;
+}
+}
+}
+}
+
+for (int i = 1; i <= n; i++) {
+Basute2[i] = 0;
+}
+
+for (int i = 1; i <= n; i++) {
+for (int j = 1; j <= n; j++) {
+Visit[i][j] = false;
+}
+}
+}
+
+for (int i = 1; i <= n; i++) {
+for (int j = 1; j <= n; j++) {
+cout << Basute[i][j] << " ";
+}
+cout << endl;
+}
+
+return 0;
+}*/
